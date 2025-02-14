b3713671ad3070cc31b1e00181301c78e370e93b
YUNHAI <bybxbwg@foxmail.com>
2025-02-14 14:46:37 +0800

    refactor: 将 task_hive 重命名为 job_hive
    - 修改了包名、模块名和文件名
    - 更新了 pyproject.toml 中的项目名称
    - 保持了原有的功能和代码逻辑不变

--- task_hive/__init__.py
+++
@@ -1,4 +0,0 @@
-from task_hive.work import HiveWork
-from task_hive.job import Job
-from task_hive.core import Status
-from task_hive.base import BaseQueue
--- task_hive/queue/__init__.py
+++
@@ -1,1 +0,0 @@
-from task_hive.queue.redis_queue import RedisQueue
