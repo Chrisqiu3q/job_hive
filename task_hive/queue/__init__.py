fdf85e42c42ab4d0caa5723b8cae91ff4f4ff9d9
YUNHAI <bybxbwg@foxmail.com>
2025-02-14 14:33:46 +0800

    feat:完善项目结构和基本功能实现
    - 新增 queue 模块和 RedisQueue 类- 更新 task_hive 模块，增加 HiveWork、Job、Status 等类
    - 添加 LICENSE 文件，使用 MIT 协议- 更新 poetry.lock 和 pyproject.toml，增加依赖和调整 Python版本
    - 新增 .github/workflows/python-package.yml，配置 CI/CD
    - 更新 README.md，添加项目说明和使用示例- 更新 tests/test.py，调整测试代码

---
+++ task_hive/queue/__init__.py
@@ -0,0 +1,1 @@
+from task_hive.queue.redis_queue import RedisQueue
--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,5 @@
 from task_hive.work import HiveWork
-from task_hive.redis_queue import RedisQueue
+from task_hive.queue.redis_queue import RedisQueue
 
 if __name__ == '__main__':
 
